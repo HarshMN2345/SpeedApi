278284e9496a43051f3bdb1b892ca8f6178a58bb
Harsh Mahajan <127186841+HarshMN2345@users.noreply.github.com>
2024-07-22 01:34:04 +0530

    complete

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,56 @@
+import { Redis } from "@upstash/redis/cloudflare";
+import { Hono } from "hono"
+import { env } from "hono/adapter";
+import { cors } from "hono/cors";
+import { handle } from "hono/vercel";
+
+export const runtime="edge"
+
+
+const app=new Hono().basePath('/api')
+app.use('/*',cors())
+
+type EnvConfig={
+    UPSTASH_REDIS_REST_URL:string
+    UPSTASH_REDIS_REST_TOKEN:string
+}
+app.get('/search',async(c)=>{
+    try{
+        const {UPSTASH_REDIS_REST_TOKEN,UPSTASH_REDIS_REST_URL}=env<EnvConfig>(c)
+        const start=performance.now();
+        const redis=new Redis({
+            url:UPSTASH_REDIS_REST_URL,
+            token:UPSTASH_REDIS_REST_TOKEN
+        })
+        const query=c.req.query("q")?.toUpperCase();
+        if (!query) return c.json({message:"Invalid Serach Query"},{status:400})
+            const res=[]
+            const rank = await redis.zrank("terms",query)
+         if(rank!==null&&rank!==null){
+            const temp=await redis.zrange("terms",rank,rank+100);
+            for (const el of temp) {
+                if (typeof el !== 'string') {
+                    continue; // Skip non-string elements
+                  }
+                if (!el.startsWith(query)) {
+                  break
+                }
+        
+                if (el.endsWith('*')) {
+                  res.push(el.substring(0, el.length - 1))
+                }
+              }
+         }
+         const end=performance.now();
+        return c.json({
+            results:res,
+            duration:end-start
+        });
+
+    }catch(error){
+       console.log(error);
+         return c.json({message:"Internal Server Error"},{status:500})
+    } 
+})
+export const GET=handle(app);
+export default app as never
